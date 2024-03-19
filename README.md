50a7adbfc884c52db5ea0db50b7d171822d9bb8b
Duc An <anmd03.bk@gmail.com>
2024-03-19 17:22:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sample Spring Batch Program
+
+## Input 
+csv with orders data
+## Ouput 
+Inserted database record
+
+# Thanks to below tutorial 
+
++ https://spring.io/guides/gs/batch-processing/#initial
+
++ https://spring.io/guides/gs/accessing-data-mysql/
+
+# Tutorial
+## Japanese 
+https://qiita.com/mytv1/items/d515acabcd73dd2358b4
+
+## Vietnamese 
+https://viblo.asia/p/sample-dung-spring-batch-de-xu-ly-du-lieu-don-hang-dang-csv-E375zQw6lGW
---
+++ src/main/java/com/example/demo/batchprocessing/Order.java
@@ -0,0 +1,70 @@
+package com.example.demo.batchprocessing;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="orders")
+public class Order {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public Integer getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public Integer getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(Integer itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(String purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	private Integer customerId;
+
+	private Integer itemId;
+	
+	private String itemName;
+
+	private Integer itemPrice;
+	
+	private String purchaseDate;
+}
+
